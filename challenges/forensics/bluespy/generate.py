68f94aca72b9067f0250ae53c1387b1e33eeb7ae
dayt0n <dayt0n@dayt0n.com>
2021-08-10 04:15:39 -0700

    added forensics/pingfail challenge

---
+++ challenges/forensics/bluespy/generate.py
@@ -0,0 +1,22 @@
+# https://thepacketgeek.com/scapy/building-network-tools/part-06/
+from scapy.all import *
+
+flag = 'UAH{it5_imp0ssibl3_T0_g3t_A_repLy!}'
+pcap = "dist/result.pcap"
+
+def do_stego(url):
+    reqStr = f'GET / HTTP/1.1\r\nHost: {url}\r\n\r\n'
+    print(f"there should be {len(flag)+1} packets")
+    waits = [ord(x) for x in list(flag)]
+    print(waits)
+    pkts = []
+    p = IP(dst=url)/ICMP()
+    p.time = 0
+    pkts.append(p)
+    for t in waits:
+        p = IP(dst=url)/ICMP()
+        p.time = t
+        pkts.append(p)
+    wrpcap(pcap,pkts)
+
+do_stego("www.uah.edu")
--- challenges/pwn/get_the_flaAAAAAAAAA/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker build -t pwn-get_the_flaaaaaaaaaa .
-echo "hosting pwn-get_the_flaaaaaaaaaa on: localhost 50371"
-docker run -it --rm -p 50371:1337 --name test-pwn-get_the_flaaaaaaaaaa pwn-get_the_flaaaaaaaaaa #/bin/bash
